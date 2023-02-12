4735ca0c8ff0cf0491389771e0e088d9ebc9b5be
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2023-02-12 13:40:40 -0500

    Add files via upload

---
+++ OrderDriver.cpp
@@ -0,0 +1,52 @@
+#include "Orders.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "test for OrderDriver...\n" << endl;
+	OrderList OrderList;
+	Deploy aDeploy;
+	Advance anadvance;
+	Bomb abomb;
+	Blockade ablockade;
+	Airlift anairlift;
+	Negotiate anegotiate;
+
+	//set order list for all
+	OrderList.set_order_list(&aDeploy);
+	OrderList.set_order_list(&anadvance);
+	OrderList.set_order_list(&abomb);
+	OrderList.set_order_list(&ablockade);
+	OrderList.set_order_list(&anairlift);
+	OrderList.set_order_list(&anegotiate);
+
+
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
+		cout <<"  "<< OrderList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+	//delete an order
+	OrderList.delete_order(&aDeploy);
+
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
+		cout << "  " << OrderList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+	//move an order
+	OrderList.move(0,4);
+
+	//OrderList.move(0, 5);
+	OrderList.move(0, 8);//invalid
+	
+	//print orderlist
+	cout << "\n the orderlist contains: " << endl;
+	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
+		cout << "  " << OrderList.get_order_list()->at(i)->get_type() << endl;
+	}
+
+}
---
+++ Orders.h
@@ -0,0 +1,103 @@
+#pragma once 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// COMP 345S - ASSIGNMENT 1
+// NAME: LE VIET CUONG, STUDENT ID: 40151142
+// : Orders are created by the player during their turn and placed into the player’s list of orders. By default, each order is placed 
+//	in the list sequentially
+
+class Order
+{
+public :
+	Order();
+	~Order();
+	//Order(const Order& O);//copy constructor
+	
+	//check of the oder is valid 
+	void validate();
+	//execute method 
+	void execute();
+	
+	//set type of the subclass
+	void set_type_id(int num);
+	string get_type();
+
+private :
+	bool valid;
+	vector<string> vec_type1 = { "deploy", "advance", "bomb", "blockade", "airlift", "negotiate" };
+	int type_id;
+};
+
+class Deploy : public Order 
+{
+
+public:
+	Deploy();
+	~Deploy();
+	string* get_type();
+
+private:
+	string type1 = { "deploy" };
+};
+
+class Advance : public Order {
+public:
+	Advance();
+	~Advance();
+
+private:
+};
+
+class Bomb : public Order 
+{
+public:
+	Bomb();
+	~Bomb();
+
+private:
+};
+
+class Blockade : public Order
+{
+public:
+	Blockade();
+	~Blockade();
+private:
+};
+
+class Airlift : public Order
+{
+public:
+	Airlift();
+	~Airlift();
+private:
+};
+
+class Negotiate : public Order
+{
+public:
+	Negotiate();
+	~Negotiate();
+
+private:
+};
+
+class OrderList
+{
+public:
+	void set_order_list(Order* an_order);
+	vector<Order*>* get_order_list();
+	
+	//delete an order
+	void delete_order(Order* oneOrder);
+	
+	//move the orders
+	void move(int position, int new_position);
+
+private:
+	vector<Order*> vec_order_list; //store the orders put in
+};
