b1bc76aadceb800d259a8ff65b52611b58c2239f
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2023-03-19 20:39:26 -0400

    Update OrderDriver.cpp

--- OrderDriver.cpp
+++ OrderDriver.cpp
@@ -4,49 +4,47 @@ using namespace std;
 
 int main()
 {
-	cout << "test for OrderDriver...\n" << endl;
-	OrderList OrderList;
-	Deploy aDeploy;
-	Advance anadvance;
-	Bomb abomb;
-	Blockade ablockade;
-	Airlift anairlift;
-	Negotiate anegotiate;
+	cout << "test OrderDriver...\n" << endl;
+	OrderList anOrderList;
 
-	//set order list for all
-	OrderList.set_order_list(&aDeploy);
-	OrderList.set_order_list(&anadvance);
-	OrderList.set_order_list(&abomb);
-	OrderList.set_order_list(&ablockade);
-	OrderList.set_order_list(&anairlift);
-	OrderList.set_order_list(&anegotiate);
+	Deploy *aDeploy=new Deploy;
+	Advance *anadvance = new Advance;
+	Bomb *abomb = new Bomb;
+	Blockade *ablockade = new Blockade;
+	Airlift *anairlift = new Airlift;
+	Negotiate *anegotiate = new Negotiate;
+
+	anOrderList.set_order_list(aDeploy);
+	anOrderList.set_order_list(anadvance);
+	anOrderList.set_order_list(abomb);
+	anOrderList.set_order_list(ablockade);
+	anOrderList.set_order_list(anairlift);
+	anOrderList.set_order_list(anegotiate);
 
 
 	//print orderlist
 	cout << "\n the orderlist contains: " << endl;
-	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
-		cout <<"  "<< OrderList.get_order_list()->at(i)->get_type() << endl;
+	for (int i = 0; i < anOrderList.get_order_list()->size(); i++) {
+		cout <<"  "<< anOrderList.get_order_list()->at(i)->get_type() << endl;
 	}
 
 	//delete an order
-	OrderList.delete_order(&aDeploy);
+	anOrderList.delete_order(aDeploy);
 
 	//print orderlist
 	cout << "\n the orderlist contains: " << endl;
-	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
-		cout << "  " << OrderList.get_order_list()->at(i)->get_type() << endl;
+	for (int i = 0; i < anOrderList.get_order_list()->size(); i++) {
+		cout << "  " << anOrderList.get_order_list()->at(i)->get_type() << endl;
 	}
 
 	//move an order
-	OrderList.move(0,4);
-
-	//OrderList.move(0, 5);
-	OrderList.move(0, 8);//invalid
-
+	anOrderList.move(0,4);
+	//anOrderList.move(0, 5);
+	anOrderList.move(0, 8);//invalid
 	//print orderlist
 	cout << "\n the orderlist contains: " << endl;
-	for (int i = 0; i < OrderList.get_order_list()->size(); i++) {
-		cout << "  " << OrderList.get_order_list()->at(i)->get_type() << endl;
+	for (int i = 0; i < anOrderList.get_order_list()->size(); i++) {
+		cout << "  " << anOrderList.get_order_list()->at(i)->get_type() << endl;
 	}
 
 }
